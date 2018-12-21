02709a38fc3106ef881afc56bace8ea6c514f6eb
Monte Tima <Monte Tima@LASER-LITE>
2018-12-21 12:20:13 -0800

    First GH from VS

---
+++ PS-Xamarin/PS-Xamarin/PS-Xamarin/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using PS_Xamarin.Models;
+
+namespace PS_Xamarin.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
